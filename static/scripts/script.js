123858575f8a90ae4abf1f9f2e972d2e4136268d
Vinícius <viniciushallmann2015@gmail.com>
2023-10-09 23:30:41 -0300

    commit muda cor de nro repetido

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -38,7 +38,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 } else {
                     this.contentEditable = true;
                 }
-                verifyNumber();
+                isValidRow(this);
+                isValidColumn(this);
                 updateSudokuStatus();
                 isSudokuComplete();
             }
@@ -113,7 +114,6 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(function(data) {
             console.log(data);
             if (data.message === 'Valid Solution') {
-                // alert("");
                 alert('Parabens voce completou o sudoku!!');
             } else if (data.message === 'Invalid Solution') {
                 alert('Solução inválida.');
@@ -121,22 +121,58 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
     
-    function verifyNumber() {
-        fetch( '/check_number', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            data: JSON.stringify({ 'row': row, 'column': column, 'value': value })
-        })
-        .then(function(data) {
-            var resultado = data.resultado;
-            if (resultado) {
-                console.log('Número válido no Sudoku.');
-            } else {
-                console.log('Número inválido no Sudoku.');
+    function isValidRow(cell){
+        var row = cell.parentNode;
+        var value = cell.textContent.trim();
+        var rowCells = Array.from(row.children);
+        var equalNumberRow = new Array();
+        for ( i = 0; i < rowCells.length; i++){
+            if (rowCells[i].textContent.trim() === value){
+                equalNumberRow.push(rowCells[i]);
             }
-        })
+        }
+        if (equalNumberRow.length > 1){
+            equalNumberRow.forEach(function(cell){
+                cell.classList.add('red-number');
+            });
+        }
+        else{
+            rowCells.forEach(function(cell){
+                cell.classList.remove('red-number');
+            });
+        }
+    }
+    function isValidColumn(cell){
+        var column = cell.parentNode;
+        var colIndex = Array.from(column.children).indexOf(cell);
+        var colCells = new Array();
+        var equalNumberCol = new Array();
+        cells.forEach(function(c) {
+            var rowIndex = Array.from(c.parentNode.children).indexOf(c);
+            if (rowIndex === colIndex) {
+                colCells.push(c);
+            }
+        });
+        for ( i = 0; i < colCells.length; i++){
+            console.log(colCells[i].textContent.trim());
+        }
+        var value = cell.textContent.trim();
+
+        for ( i = 0; i < colCells.length; i++){
+            if (colCells[i].textContent.trim() === value){
+                equalNumberCol.push(colCells[i]);
+            }
+        }
+        if (equalNumberCol.length > 1){
+            equalNumberCol.forEach(function(cell){
+                cell.classList.add('red-number');
+            });
+        }
+        else{
+            colCells.forEach(function(cell){
+                cell.classList.remove('red-number');
+            });
+        }
     }
     
     updateSudokuStatus();
